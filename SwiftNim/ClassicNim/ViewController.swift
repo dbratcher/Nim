1bc33601ea381ea4e21cd98cb48e2811f3b9cf1b
Drew Bratcher <dbratcher@gatech.edu>
2019-04-03 18:31:46 -0700

    started swift version of nim

---
+++ SwiftNim/ClassicNim/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClassicNim
+//
+//  Created by Drew Bratcher on 4/3/19.
+//  Copyright © 2019 Drew Bratcher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
