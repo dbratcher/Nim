0a92858c15c74f8d6fb73fe073037945a52acd26
Drew Bratcher <dbratcher@gatech.edu>
2019-04-05 19:07:05 -0700

    Start work on settings screen

---
+++ SwiftNim/ClassicNim/View Controllers/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  ClassicNim
+//
+//  Created by Drew Bratcher on 4/5/19.
+//  Copyright © 2019 Drew Bratcher. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+}
