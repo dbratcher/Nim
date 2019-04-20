23face0d8035dc92696c64b1a9d176c8380d09fe
Drew Bratcher <dbratcher@gatech.edu>
2019-04-20 16:57:12 -0700

    Created LabeledStepper class for customize board control

---
+++ SwiftNim/ClassicNim/Settings/BoardCustomization/LabeledStepper.swift
@@ -0,0 +1,71 @@
+//
+//  LabeledStepper.swift
+//  ClassicNim
+//
+//  Created by Drew Bratcher on 4/20/19.
+//  Copyright © 2019 Drew Bratcher. All rights reserved.
+//
+
+import UIKit
+
+protocol LabelStepperDelegate: class {
+    func valueChanged(_ newValue: Int)
+}
+
+@IBDesignable
+class LabeledStepper: UIStackView {
+    private let label = UILabel(frame: .zero)
+    private let countLabel = UILabel(frame: .zero)
+    private let stepper = UIStepper(frame: .zero)
+    
+    private let maxValue = 7
+    private let minValue = 0
+    private let font = UIFont(name: "MarkerFelt-Thin", size: 24)
+    private let color = UIColor.white
+    
+    public weak var delegate: LabelStepperDelegate?
+    
+    @IBInspectable
+    var text: String = "Stepper:" {
+        didSet {
+            label.text = text
+            label.isHidden = text.isEmpty
+        }
+    }
+    
+    @IBInspectable
+    var value: Int = 1 {
+        didSet {
+            countLabel.text = "\(value)"
+            stepper.value = Double(value)
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    private func setupView() {
+        addArrangedSubview(label)
+        addArrangedSubview(countLabel)
+        addArrangedSubview(stepper)
+        
+        label.font = font
+        label.textColor = color
+        countLabel.font = font
+        countLabel.textColor = color
+        countLabel.textAlignment = .center
+        stepper.addTarget(self, action: #selector(valueChanged), for: .valueChanged)
+    }
+    
+    @objc func valueChanged() {
+        value = Int(stepper.value)
+        delegate?.valueChanged(value)
+    }
+}
