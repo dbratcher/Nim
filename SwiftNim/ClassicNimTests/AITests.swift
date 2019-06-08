048149d78b95b2bbfb0f7da5a40282cf419f1e8f
Drew Bratcher <dbratcher@gatech.edu>
2019-06-08 16:47:50 -0700

    finish ai tests, closes #8

--- SwiftNim/ClassicNimTests/AITests.swift
+++ SwiftNim/ClassicNimTests/AITests.swift
@@ -20,7 +20,33 @@ class AIEngineTests: XCTestCase {
 
         let (stones, stack) = easyAI.nextMove(for: board)
         XCTAssert(stones > 0)
-        XCTAssert(stones <= 5)
+        XCTAssert(stones < 5)
         XCTAssert((stack.identifier == stack1.identifier) || (stack.identifier == stack2.identifier))
+    }
+
+    func testMediumAI() {
+        let mediumAI = AIEngine(difficulty: .medium)
+
+        let stack1 = Stack(identifier: UUID(), stoneCount: 4)
+        let stack2 = Stack(identifier: UUID(), stoneCount: 4)
+        let board = GameBoard(stacks: [stack1, stack2])
+
+        let (stones, stack) = mediumAI.nextMove(for: board)
+        XCTAssert(stones > 0)
+        XCTAssert(stones < 5)
+        XCTAssert((stack.identifier == stack1.identifier) || (stack.identifier == stack2.identifier))
+    }
+
+    func testHardAI() {
+        let hardAI = AIEngine(difficulty: .hard)
+
+        let stack1 = Stack(identifier: UUID(), stoneCount: 3)
+        let stack2 = Stack(identifier: UUID(), stoneCount: 5)
+        let stack3 = Stack(identifier: UUID(), stoneCount: 5)
+        let board = GameBoard(stacks: [stack1, stack2, stack3])
+
+        let (stones, stack) = hardAI.nextMove(for: board)
+        XCTAssert(stones == 3)
+        XCTAssert(stack.identifier == stack1.identifier)
     }
 }
